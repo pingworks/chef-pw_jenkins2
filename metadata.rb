cf95161a7c37f87d246075183dd2d9f2ef2a39ed
Alexander Birk <birk@pingworks.de>
2017-02-26 10:32:21 +0100

    jenkins 2.47 BlueOcean on Ubuntu 16.04

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'christoph.lukas@gmx.net'
 license          'All rights reserved'
 description      'Installs/Configures jenkins'
 long_description 'Installs/Configures jenkins'
-version          '0.0.3'
+version          '0.0.4'
 
 depends 'apt'
 depends 'ark'
